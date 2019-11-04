70ee6117b67cf78a38bddce331fbebbfc018fe8e
Taishawn King <taishawnking1@yahoo.com>
2019-11-04 16:39:24 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,12 +1,6 @@
 # Use this space to code along with the readme
 def say_phrase_ten-times
-  phrase = "Hello World!"
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
+  puts "Hello World!"
 end
 
 say_phrase_ten-times
